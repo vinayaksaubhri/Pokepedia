de1883ac1314856760ea4eeb4424b479d33ad9dc
vinayaksaubhri <vinayaksaubhri@gmail.com>
2023-05-22 13:18:36 +0530

    feat:added heading and sub heading Text in home

--- screens/home/index.tsx
+++ screens/home/index.tsx
@@ -1,11 +1,18 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import CustomSafeAreaView from "../../components/customSafeAreaView";
 import SearchBar from "../../components/searchBar";
+import { COLORS, FONTS } from "../../style/style";
+import { moderateScale, scaleFont, verticalScale } from "../../style/metrics";
 
 const Home = () => {
   return (
     <CustomSafeAreaView>
       <View style={styles.container}>
+        <Text style={styles.headingTextStyle}>Pokédex</Text>
+        <Text style={styles.subHeadingTextStyle}>
+          Use the advanced search to find Pokémon by type, weakness, ability and
+          more!
+        </Text>
         <SearchBar showFilter />
       </View>
     </CustomSafeAreaView>
@@ -17,6 +24,21 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     backgroundColor: "white",
-    padding: 24,
+    padding: moderateScale(24),
+  },
+  headingTextStyle: {
+    alignSelf: "flex-start",
+    fontSize: scaleFont(36),
+    color: COLORS.primaryBlue,
+    fontFamily: FONTS.RC_Regular,
+    marginBottom: verticalScale(4),
+  },
+  subHeadingTextStyle: {
+    alignSelf: "flex-start",
+    fontSize: scaleFont(16),
+    color: COLORS.primaryBlue,
+    fontFamily: FONTS.RC_Regular,
+    marginBottom: verticalScale(16),
+    lineHeight: verticalScale(24),
   },
 });
