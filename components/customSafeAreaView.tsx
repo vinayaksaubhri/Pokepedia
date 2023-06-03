30427710ed8105c5c7a3de00e04b73eab633f95c
vinayaksaubhri <vinayaksaubhri@gmail.com>
2023-06-04 00:16:04 +0530

    feat:safe area context to customSafeArea

--- components/customSafeAreaView.tsx
+++ components/customSafeAreaView.tsx
@@ -1,15 +1,12 @@
 import { ReactNode } from "react";
 import {
   Platform,
-  SafeAreaView,
   StyleSheet,
   StatusBar,
   KeyboardAvoidingView,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { COLORS } from "../style/style";
-
-const STATUSBAR_HEIGHT =
-  Platform.OS === "android" ? StatusBar.currentHeight - 12 : 0;
 
 type Props = {
   children?: ReactNode | null;
@@ -20,12 +17,13 @@ const CustomSafeAreaView: React.FC<Props> = ({ children }) => {
     container: {
       flex: 1,
       backgroundColor: COLORS.surface,
-      paddingTop: STATUSBAR_HEIGHT,
     },
   });
   return (
     <KeyboardAvoidingView style={styles.container} behavior="height">
-      <SafeAreaView style={styles.container}>{children}</SafeAreaView>
+      <SafeAreaView edges={["right", "left", "top"]} style={styles.container}>
+        {children}
+      </SafeAreaView>
     </KeyboardAvoidingView>
   );
 };
