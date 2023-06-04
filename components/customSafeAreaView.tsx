083f4691c45af3b12cd1e4b451043efec4ed9805
vinayaksaubhri <vinayaksaubhri@gmail.com>
2023-06-04 22:58:16 +0530

    feat:added custom status bar to custom safe area view

--- components/customSafeAreaView.tsx
+++ components/customSafeAreaView.tsx
@@ -1,18 +1,23 @@
 import { ReactNode } from "react";
+import { KeyboardAvoidingView, StatusBar, StyleSheet } from "react-native";
 import {
-  Platform,
-  StyleSheet,
-  StatusBar,
-  KeyboardAvoidingView,
-} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { COLORS } from "../style/style";
+  NativeSafeAreaViewProps,
+  SafeAreaView,
+} from "react-native-safe-area-context";
+import { COLORS, POKEMON_COLOR } from "../style/style";
+import CustomStatusBar from "./customStatusBar";
 
 type Props = {
   children?: ReactNode | null;
+  edges?: NativeSafeAreaViewProps["edges"];
+  backgroundColor?: string;
 };
 
-const CustomSafeAreaView: React.FC<Props> = ({ children }) => {
+const CustomSafeAreaView: React.FC<Props> = ({
+  children,
+  edges = ["right", "left"],
+  backgroundColor = "#fff",
+}) => {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -21,7 +26,8 @@ const CustomSafeAreaView: React.FC<Props> = ({ children }) => {
   });
   return (
     <KeyboardAvoidingView style={styles.container} behavior="height">
-      <SafeAreaView edges={["right", "left", "top"]} style={styles.container}>
+      <CustomStatusBar backgroundColor={backgroundColor} />
+      <SafeAreaView edges={edges} style={styles.container}>
         {children}
       </SafeAreaView>
     </KeyboardAvoidingView>
