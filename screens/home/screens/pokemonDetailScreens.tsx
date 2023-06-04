a1f0984a6e84dbd78b58824ec1368ec160f67411
vinayaksaubhri <vinayaksaubhri@gmail.com>
2023-06-04 15:07:45 +0530

    feat:added pokemon detail screen

---
+++ screens/home/screens/pokemonDetailScreens.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+const PokemonDetailScreens = () => {
+  return (
+    <View>
+      <Text>PokemonDetailScreens</Text>
+    </View>
+  );
+};
+export default PokemonDetailScreens;
+const styles = StyleSheet.create({});
