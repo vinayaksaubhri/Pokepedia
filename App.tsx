2c5fdd87e8d6a655b7cb1298944ff81f760b6464
vinayaksaubhri <vinayaksaubhri@gmail.com>
2023-07-06 15:49:56 +0530

    feat:removes warnings from the ap

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import "react-native-gesture-handler";
 import { LoadAssets } from "./components/loadAssets";
 import Navigation from "./navigation";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { LogBox } from "react-native";
+LogBox.ignoreLogs(["Warning: ..."]); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 export default function App() {
   const Fonts = {
