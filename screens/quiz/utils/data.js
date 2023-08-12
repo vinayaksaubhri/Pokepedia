6498c73886e7d63b358f2cc6fb457a61119341c0
vinayaksaubhri <vinayaksaubhri@gmail.com>
2023-08-13 02:08:38 +0530

    feat:utility function for quiz

---
+++ screens/quiz/utils/data.js
@@ -0,0 +1,24 @@
+import { list as pokemonList } from "./pokemonListQuiz";
+const min = 1;
+const max = 1273;
+
+function getRandomPokemon() {
+  const result = [];
+
+  //get 3 random numbers
+  while (result.length < 3) {
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (!result.includes(randomNumber)) {
+      result.push(randomNumber);
+    }
+  }
+
+  //get pokemon from pokemonList using random numbers
+  return result.map((number) => pokemonList[number]);
+}
+function selectCurrentPokemon(pokemonSelectedList) {
+  const randomIndex = Math.floor(Math.random() * pokemonSelectedList.length);
+  return pokemonSelectedList[randomIndex];
+}
+
+export { getRandomPokemon, selectCurrentPokemon };
