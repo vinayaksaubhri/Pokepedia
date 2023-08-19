b493bd71ace483bfbbefeafbbdf622108e7dfd02
vinayaksaubhri <vinayaksaubhri@gmail.com>
2023-08-19 15:44:17 +0530

    feat: no internet screen

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,9 +1,15 @@
+import NoInternet from "../components/noInternet";
+import useNetworkInfo from "../hooks/useNetworkInfo";
 import AuthorizedNavigation from "./authorizedNavigation";
 import UnAuthorizedNavigation from "./unAuthorizedNavigation";
 
 const user = true;
 
 const Navigation = () => {
+  const [isOnline, onPressReload] = useNetworkInfo();
+  if (!isOnline) {
+    return <NoInternet onPressReload={onPressReload} />;
+  }
   if (user) return <AuthorizedNavigation />;
 
   return <UnAuthorizedNavigation />;
