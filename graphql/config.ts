c830a1619b2de31ff77fead8887d418a551290f8
vinayaksaubhri <vinayaksaubhri@gmail.com>
2024-08-18 02:43:24 +0530

    feat:changed envs

--- graphql/config.ts
+++ graphql/config.ts
@@ -1,14 +1,19 @@
 import { GraphQLClient } from "graphql-request";
 
-if (typeof process.env.HASURA_GRAPHQL_ENDPOINT === "undefined") {
-  throw new Error("HASURA_GRAPHQL_ENDPOINT is not defined");
+if (
+  typeof process.env.EXPO_PUBLIC_HASURA_GRAPHQL_ENDPOINT === "undefined" ||
+  typeof process.env.EXPO_PUBLIC_HASURA_GRAPHQL_ADMIN_SECRET === "undefined"
+) {
+  throw new Error(
+    "HASURA_GRAPHQL_ENDPOINT or EXPO_PUBLIC_HASURA_GRAPHQL_ADMIN_SECRET is not defined"
+  );
 }
 
-const endpoint = process.env.HASURA_GRAPHQL_ENDPOINT;
+const endpoint = process.env.EXPO_PUBLIC_HASURA_GRAPHQL_ENDPOINT;
 const client = new GraphQLClient(endpoint);
 client.setHeader(
   "x-hasura-admin-secret",
-  "4to7NQKLeOaHh3nXrx5fgQJL84R9g5EnO36AebUoTkw9DDglcb1Z3ZbugAec3BgB"
+  process.env.EXPO_PUBLIC_HASURA_GRAPHQL_ADMIN_SECRET
 );
 
 export default client;
